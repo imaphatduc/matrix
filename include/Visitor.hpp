6f3c87d6900b3908f8bbc8540530a0de3d9a582f
imaphatduc <vutranducphat@gmail.com>
2022-05-09 12:52:36 +0700

    feat(visitor): Add different types of AST nodes that the visitors can visit.
    (Note: LLVMVisitor code will be pushed in the next commit).

--- include/Visitor.hpp
+++ include/Visitor.hpp
@@ -1,12 +1,20 @@
 #ifndef VISITOR_HPP
 #define VISITOR_HPP
 
+#include "BinaryExprNode.hpp"
+#include "DoubleNode.hpp"
 #include "IntegerNode.hpp"
+#include "ProgramNode.hpp"
+#include "StatementNode.hpp"
 #include <iostream>
 
 class Visitor {
     public:
+        virtual void visit(ProgramNode *node);
+        virtual void visit(StatementNode *node);
         virtual void visit(IntegerNode *node);
+        virtual void visit(DoubleNode *node);
+        virtual void visit(BinaryExprNode *node);
 };
 
 #endif
--- src/Visitor.cpp
+++ src/Visitor.cpp
@@ -1,6 +1,23 @@
 #include "../include/Visitor.hpp"
 
+void Visitor::visit(ProgramNode *node)
+{
+}
+
+void Visitor::visit(StatementNode *node)
+{
+}
+
 void Visitor::visit(IntegerNode *node)
 {
     std::cout << node->getValue() << ' ';
+}
+
+void Visitor::visit(DoubleNode *node)
+{
+    std::cout << node->getValue() << ' ';
+}
+
+void Visitor::visit(BinaryExprNode *node)
+{
 }
